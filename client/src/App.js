0fc045ea7872101a6a3271423c16859159ea97b5
Mamillapalli Jyothika <109164392+Jyo2253@users.noreply.github.com>
2026-01-03 21:41:53 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io(process.env.REACT_APP_SOCKET_URL);
 
 function App() {
   const [username, setUsername] = useState("");
